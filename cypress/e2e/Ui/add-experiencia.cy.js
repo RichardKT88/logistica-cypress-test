c982287c2d942fe0d81994f0cdba1403c38961dd
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-08-31 14:53:10 -0300

    adicionando cenário negativo em add-experiencia

--- cypress/e2e/Ui/add-experiencia.cy.js
+++ cypress/e2e/Ui/add-experiencia.cy.js
@@ -15,7 +15,7 @@ describe('Funcionalidade: Adicionar experiência', () => {
         cy.get('[data-test="experience-delete"]').should('exist')
     });
 
-    it.only('Deve adicionar uma experiência Atual com sucesso', () => {
+    it('Deve adicionar uma experiência Atual com sucesso', () => {
         experienciaPage.addExperienciaAtual('QA', 'Via', 'SP', '01/01/2020','ViaHub é top')
         cy.get('[data-test="experience-delete"]').should('exist')
     });
@@ -24,5 +24,13 @@ describe('Funcionalidade: Adicionar experiência', () => {
         experienciaPage.addExperiencia('QA', 'VIA', 'São Paulo', '01/01/2020', '01/01/2025', 'Mussum ipsum')
         cy.get('[data-test="experience-delete"]').first().click()
         cy.contains('Experiência Removida').should('be.visible')
+    });
+
+    it.only('Validar mensagem de campos obrigatórios quando não preenchidos', () => {
+        cy.get('[data-test="experience-submit"]').click()
+        cy.get('.MuiFormHelperText-root').should('contain', 'Posição é obrigatória')
+        cy.get('.MuiFormHelperText-root').should('contain', 'Empresa é obrigatória')
+        cy.get('.MuiFormHelperText-root').should('contain', 'Início é obrigatório')
+        cy.get('.MuiFormHelperText-root').should('contain', 'Até é obrigatório')
     });
 });
